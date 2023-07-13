baf5b3d16bb1d57c071e8369284cf5c005df57e6
unknown <cm17164@ues.edu.sv>
2023-07-12 21:20:08 -0600

    COMMIT UNO GUARDAR ARCHIVOS

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>CM17164 Prueba Docker File</h1>
+</body>
+</html>
